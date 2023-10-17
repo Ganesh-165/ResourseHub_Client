f43222fceaf287a4603cc45220f1df5b95cc956f
Ganesh <vtganesh165@gmail.com>
2023-10-17 19:00:05 +0530

    second commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:'https://resouse-hub-server.onrender.com',
+    baseURL:'https://resorse-hub-server.onrender.com',
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 });
-
-//https://resouse-hub-server.onrender.com
-// https://resorse-hub-server.onrender.com
